aa3c67748f683d610944540bbdb15c64af9d98e0
haldryn <canales16086@iesmarenostrum.com>
2020-02-18 19:25:53 +0100

    scroll a 200

--- inc/js/parriba.js
+++ inc/js/parriba.js
@@ -1,7 +1,7 @@
 $("#parriba").css("display", "none");
 
 $(window).scroll(function() {
-    if ($(window).scrollTop() > 500) {
+    if ($(window).scrollTop() > 200) {
         $("#parriba").fadeIn("slow");
     } else {
         $("#parriba").fadeOut("slow");
